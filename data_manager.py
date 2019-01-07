7593d0b2cb8330b88d04d081af2debc3e72e9e17
AnnamaryPethe <41436287+AnnamaryPethe@users.noreply.github.com>
2019-01-07 10:57:50 +0100

    add main, data_manager

---
+++ data_manager.py
@@ -0,0 +1,71 @@
+import connection
+from werkzeug.security import check_password_hash
+import psycopg2
+
+
+@connection.connection_handler
+def register_new_user(cursor, username, password, email):
+    try:
+        cursor.execute("""
+                        SELECT * FROM users
+                        WHERE email = %(email)s OR user_name = %(username)s;
+                        """, {'email': email, 'username': username})
+        user = cursor.fetchall()
+        if user != []:
+            return False
+        else:
+            cursor.execute("""INSERT INTO users (user_name, hashed_pw, email)
+                              VALUES (%(name)s, %(pw)s, %(email)s);""",
+                           {'name': username, 'pw': password, 'email': email})
+            return True
+    except psycopg2.IntegrityError:
+        return False
+
+
+@connection.connection_handler
+def user_login(cursor, email, password):
+    cursor.execute("""
+                    SELECT * FROM users
+                    WHERE email = %(email)s;
+                    """, {'email': email})
+    user = cursor.fetchone()
+    if user is not None:
+        return check_password_hash(user.get('hashed_pw'), password), user.get('id'), user.get('user_name')
+    return [False]
+
+
+@connection.connection_handler
+def get_board_tree(cursor, user_id):
+    cursor.execute("""SELECT * FROM boards
+                      WHERE private = 0 OR user_id = %(user_id)s
+                      ORDER by private DESC""", {'user_id': user_id})
+    boards = cursor.fetchall()
+    for board in boards:
+        cursor.execute("""SELECT * FROM board_columns
+                          WHERE board_id = %(board_id)s""", {'board_id': board.get('id')})
+        board['columns'] = cursor.fetchall()
+        for board_column in board['columns']:
+            cursor.execute("""SELECT * FROM cards
+                              WHERE board_column_id = %(board_column_id)s
+                              ORDER BY position ASC""", {'board_column_id': board_column.get('id')})
+            board_column['cards'] = cursor.fetchall()
+    return boards
+
+
+@connection.connection_handler
+def register_new_board(cursor, board_name, board_type, user_id):
+    if board_type == 'private':
+        board_type = 1
+    else:
+        board_type = 0
+    cursor.execute("""INSERT INTO boards (title, private, user_id)
+                      VALUES (%(board_name)s, %(board_type)s, %(user_id)s)""",
+                   {'board_name': board_name,
+                    'board_type': board_type,
+                    'user_id': user_id})
+
+
+@connection.connection_handler
+def add_new_column(cursor, column_name, board_id):
+    cursor.execute("""INSERT INTO board_columns (column_name, board_id)
+                      VALUES (%(column_name)s, %(board_id)s)""", {'column_name': column_name, 'board_id': board_id})
---
+++ main.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, session, redirect, url_for, request
+from functools import wraps
+from werkzeug.security import generate_password_hash
+import data_manager
+import json
+
+app = Flask(__name__)
+app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    session.clear()
+    return redirect(url_for('boards'))
+
+
+@app.route('/board', methods=['POST'])
+def register_new_board():
+    new_board_name = request.json.get('boardName')
+    new_board_type = request.json.get('boardType')
+    user_id = session.get('user_id')
+    if user_id is None:
+        user_id = 1001
+    data_manager.register_new_board(new_board_name, new_board_type, user_id)
+    return json.dumps({'attempt': 'successful'})
+
+
+@app.route('/column', methods=['POST'])
+def add_new_column():
+    column_name = request.json.get('columnName')
+    board_id = request.json.get('boardId')
+    data_manager.add_new_column(column_name, board_id)
+    return json.dumps({'attempt': 'successful'})
+
+
+@app.route("/")
+def boards():
+    ''' this is a one-pager which shows all the boards and cards '''
+    if 'logged_in' in session:
+        status = f'Logged in as {session["username"]}'
+    else:
+        status = 'Not logged in'
+    board_tree = data_manager.get_board_tree(session.get('user_id'))
+    return render_template('boards.html', status=status, board_tree=board_tree)
+
+
+@app.route('/login_and_register', methods=['POST'])
+def login_and_register():
+    data = request.get_json(force=True)
+    if data['status'] == 'Login':
+        check_user = data_manager.user_login(data['email'], data['password'])
+        if check_user[0] is True:
+            session['logged_in'] = True
+            session['username'] = check_user[2]
+            session['user_id'] = check_user[1]
+            return 'success'
+        elif check_user[0] is False:
+            return 'failed login'
+    else:
+        password = generate_password_hash(data['password'])
+        check_user = data_manager.register_new_user(data['username'], password, data['email'])
+        if check_user is True:
+            check_user = data_manager.user_login(data['email'], data['password'])
+            print(check_user)
+            session['logged_in'] = True
+            session['username'] = check_user[2]
+            session['user_id'] = check_user[1]
+            return 'success'
+        elif check_user is False:
+            return 'failed registration'
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
